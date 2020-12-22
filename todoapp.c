b85e84ff01f495e25af170dad53be73bf22ed914
mdedmonds <mdedmonds@outlook.com>
2020-12-22 14:25:10 -0600

    laying groundwork for GUI

--- todoapp.c
+++ todoapp.c
@@ -1,24 +1,29 @@
 #include <gtk/gtk.h>
 
-static void activate (GtkApplication* app, gpointer user_data)
+int
+main (int   argc,
+      char *argv[])
 {
-  GtkWidget *window;
+  GtkBuilder *builder;
+  GObject *window;
+  GError *error = NULL;
 
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "We did it boys");
-  gtk_window_set_default_size (GTK_WINDOW (window), 400, 400);
-  gtk_widget_show_all (window);
-}
+  gtk_init (&argc, &argv);
 
-int main (int argc, char **argv)
-{
-  GtkApplication *app;
-  int status;
+  /* Construct a GtkBuilder instance and load our UI description */
+  builder = gtk_builder_new ();
+  if (gtk_builder_add_from_file (builder, "builder.ui", &error) == 0)
+    {
+      g_printerr ("Error loading file: %s\n", error->message);
+      g_clear_error (&error);
+      return 1;
+    }
 
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
-  g_object_unref (app);
+  // Set the app to quit when the window is closed
+  window = gtk_builder_get_object(builder, "window");
+  g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-  return status;
+  gtk_main ();
+
+  return 0;
 }
